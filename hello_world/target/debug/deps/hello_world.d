79dded83dbe168286faccce7310d527e63eaa1c8
Mr.B <bratickbro@gmail.com>
2026-08-17 15:01:08 +0530

    Delete hello_world directory

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\bradb\OneDrive\Desktop\New Age Subjetcs\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-C:\Users\bradb\OneDrive\Desktop\New Age Subjetcs\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-src\main.rs:
